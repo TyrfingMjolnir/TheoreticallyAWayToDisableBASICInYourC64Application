4991a52d62f0bd0e688e951a7dffdc1aebe7e67a
Tyrfing Mjølner <4lphamal3@gmail.com>
2021-12-13 01:56:27 +0100

    Update d1000.s

--- d1000.s
+++ d1000.s
@@ -1,3 +1,4 @@
+// According to this: https://sta.c64.org/cbm64mem.html as mentioned in the comments here: https://youtu.be/NKOucB0o4YQ
 DEC $0001 // Disable BASIC ROM to ENABLE RAM in the same memory space
 /// Do Whatever you would like also in the space of 0xA000 through 0xBFFF
 INC $0001 // Enable BASIC because my application is done running
